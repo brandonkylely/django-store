24fea3f70f167c6dae1479201a0eb98cee1f750b
Brandon <brandonkly@ucla.edu>
2023-08-09 18:43:13 -0700

    view function hello

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# request -> view function -> response
+# view function is a request handler
+# not to be confused with templates
+
+def say_hello(request):
+  # pull data from the database
+  # transform data
+  # send emails
+    return HttpResponse("Hello World")
+    # return render(request, "hello.html")
